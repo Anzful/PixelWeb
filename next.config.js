b00f630a428d6bfbb462932f61798eab5a43cfdf
SetFodi <lukafartenadze2004@gmail.com>
2026-05-22 21:10:26 +0400

    seo next

--- next.config.js
+++ next.config.js
@@ -8,6 +8,21 @@ const nextConfig = {
       },
     ],
   },
+  async redirects() {
+    return [
+      {
+        source: '/:path*',
+        has: [
+          {
+            type: 'host',
+            value: 'www.pixelweb.ge',
+          },
+        ],
+        destination: 'https://pixelweb.ge/:path*',
+        permanent: true,
+      },
+    ]
+  },
 }
 
 module.exports = nextConfig
